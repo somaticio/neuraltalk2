502de65a78a6d820dc54d3d7a1902509146516fc
jason toy <jasontoy@gmail.com>
2016-01-21 15:26:31 +0000

    comment out init for now

--- init.sh
+++ init.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-mkdir -p /data/models
-mkdir -p /data/images
+#mkdir -p /data/models
+#mkdir -p /data/images
 
-cd /tmp
+#cd /tmp
 # download and unzip the pretrained CPU model
-axel -n 10 http://cs.stanford.edu/people/karpathy/neuraltalk2/checkpoint_v1_cpu.zip
-unzip /tmp/checkpoint_v1_cpu.zip -d /data/models
+#axel -n 10 http://cs.stanford.edu/people/karpathy/neuraltalk2/checkpoint_v1_cpu.zip
+#unzip /tmp/checkpoint_v1_cpu.zip -d /data/models
 
 # get GPU model checkpoint
-axel -n 10 http://cs.stanford.edu/people/karpathy/neuraltalk2/checkpoint_v1.zip
-unzip /tmp/checkpoint_v1.zip -d /data/models
+#axel -n 10 http://cs.stanford.edu/people/karpathy/neuraltalk2/checkpoint_v1.zip
+#unzip /tmp/checkpoint_v1.zip -d /data/models
 
